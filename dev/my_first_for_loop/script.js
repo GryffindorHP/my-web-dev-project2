9c7b8d2f27a38e903b4b259f877e47e8062ae72f
Tiffany Clark <TClark@smartts.net>
2022-02-25 15:36:26 -0600

    add files

---
+++ dev/my_first_for_loop/script.js
@@ -0,0 +1,11 @@
+let devsArray = ["Max","Adam","Tiffany", "Jean", "Chris","Ivan",];
+for (let i = 0; i < devsArray.length-1; i++){
+ console.log(`${devsArray[i]} is a junior frontend web developer, but ${devsArray[4]} is better `);  
+ console.log(devsArray[i] + " is a junior frontend web developer, but " + devsArray[4] + " is better" );  
+}
+console.log(devsArray.length);
+console.log(devsArray[1]);
+console.log(devsArray[2]);
+console.log(devsArray[3]);
+console.log(devsArray[4]);
+
